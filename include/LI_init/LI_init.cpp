f078b2c6a282abf8c98ebe8e8e6b3d23f7f50fbc
yangfengzzz <yangfengzzz@hotmail.com>
2026-04-10 12:48:02 +0800

    Fix Jazzy build compatibility

--- include/LI_init/LI_init.cpp
+++ include/LI_init/LI_init.cpp
@@ -321,7 +321,7 @@ void LI_Init::solve_Rotation_only() {
     R_LI_quat[2] = 0;
     R_LI_quat[3] = 0;
 
-    ceres::LocalParameterization *quatParam = new ceres::QuaternionParameterization();
+    ceres::Manifold *quatParam = new ceres::QuaternionManifold();
     ceres::Problem problem_rot;
     problem_rot.AddParameterBlock(R_LI_quat, 4, quatParam);
 
@@ -357,7 +357,7 @@ void LI_Init::solve_Rot_bias_gyro(double &timediff_imu_wrt_lidar) {
 
     double time_lag2 = 0; //Second time lag (IMU wtr Lidar)
 
-    ceres::LocalParameterization *quatParam = new ceres::QuaternionParameterization();
+    ceres::Manifold *quatParam = new ceres::QuaternionManifold();
     ceres::Problem problem_ang_vel;
 
     problem_ang_vel.AddParameterBlock(R_LI_quat, 4, quatParam);
@@ -420,7 +420,7 @@ void LI_Init::solve_trans_biasacc_grav() {
     Trans_IL[1] = 0.0;
     Trans_IL[2] = 0.0;
 
-    ceres::LocalParameterization *quatParam = new ceres::QuaternionParameterization();
+    ceres::Manifold *quatParam = new ceres::QuaternionManifold();
     ceres::Problem problem_acc;
 
     problem_acc.AddParameterBlock(R_GL0_quat, 4, quatParam);
